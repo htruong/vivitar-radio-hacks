29bc0394b7c3f998cfa0c6681217717e77890bca
Huan Truong <htruong@tnhh.net>
2020-06-14 16:46:50 -0700

    Rename, refactor variables names

--- vivitarLCD.ino
+++ vivitarLCD.ino
@@ -1,17 +1,15 @@
 #include "LowPower.h"
-#include <HID-Project.h>
+#include "HID-Project.h"
 
 volatile unsigned long _millis = 0;
 
+static const byte SER_CMD_FIXED_LENGTH = 6;
+static const byte SER_BYTE_START_MAGIC = 'C';
+static const byte SER_BYTE_ACK_OK = 'K';
+static const byte SER_BYTE_ACK_FAIL = 'F';
+byte SER_cmd[SER_CMD_FIXED_LENGTH];
 
-
-static const byte SERIAL_CMD_FIXED_LENGTH = 6;
-static const byte SERIAL_BYTE_START_MAGIC = 'C';
-static const byte SERIAL_BYTE_ACK_OK = 'K';
-static const byte SERIAL_BYTE_ACK_FAIL = 'F';
-byte serial_cmd[SERIAL_CMD_FIXED_LENGTH];
-
-enum serial_state_enum {
+enum SER_state_enum {
   BYTE_START_MAGIC,
   BYTE_CMD,
   BYTE_PARAM_1,
@@ -20,36 +18,33 @@ enum serial_state_enum {
   BYTE_PARAM_4
 };
 
+volatile byte SER_state = BYTE_START_MAGIC;
 
-volatile byte serial_state = BYTE_START_MAGIC;
+bool SCR_spin_circle = false;
+bool SCR_blink_colon = false;
+bool SCR_blink_dot   = false;
+bool SCR_draw_clock  = false;
 
-bool screen_spin_circle = false;
-bool screen_blink_colon = false;
-bool screen_blink_dot   = false;
-bool screen_draw_clock  = false;
+static const byte SCR_LED_PINS_COUNT = 7;
+static const byte SCR_SEGMENTS = 42;
+static const byte SCR_DIGIT_LED_SEGMENTS = 7;
 
-static const byte LED_PINS_COUNT = 7;
-static const byte SCREEN_SEGMENTS = 42;
-static const byte DIGIT_LED_SEGMENTS = 7;
-
-static const byte PORTE_POS = 6;
-static const byte PORTE_POS_MASK = 1 << 6;
-static const byte SCREEN_SCANLINE_MS = 1;
+static const byte SCR_PORTE_POS = 6;
+static const byte SCR_PORTE_POS_MASK = 1 << 6;
+static const byte SCR_SCANLINE_MS = 1;
 
 // Due to the way that the Arduino Pro micro is laid out
 // We almost have every pin on the same PORT, but then we can't
 // First pin has to be on port PE6
 // Do not change this, if you change this,
-// The draw_screen_fast() won't work correctly no more
-byte ledPins[LED_PINS_COUNT] = {7, 15, 16, 14, 8, 9, 10};
+// The SCR_draw_fast() won't work correctly no more
+byte SCR_led_pins[SCR_LED_PINS_COUNT] = {7, 15, 16, 14, 8, 9, 10};
 
-byte indicator_led = 17;
+volatile unsigned int SCR_current_scan_i = 0;
+volatile unsigned int SCR_current_scan_offset = 0;
+volatile bool SCR_buf[SCR_SEGMENTS];
 
-volatile unsigned int current_scan_i = 0;
-volatile unsigned int current_scan_offset = 0;
-volatile bool screen_buf[SCREEN_SEGMENTS];
-
-byte screen_segment[LED_PINS_COUNT * LED_PINS_COUNT] ={
+byte SCR_segment[SCR_LED_PINS_COUNT * SCR_LED_PINS_COUNT] ={
   00, 12, 13, 16,  3,  4,  8,
   17, 00, 19, 20, 23, 22, 11,
   18, 24, 00, 26, 28,  2,  6,
@@ -59,7 +54,7 @@ byte screen_segment[LED_PINS_COUNT * LED_PINS_COUNT] ={
    9,  0,  5, 10, 34, 35, 00 
 };
 
-bool digit_segments[][DIGIT_LED_SEGMENTS] = {
+bool SCR_digit_segments[][SCR_DIGIT_LED_SEGMENTS] = {
   { 1,1,1,1,1,1,0 },
   { 0,1,1,0,0,0,0 },
   { 1,1,0,1,1,0,1 },
@@ -73,44 +68,47 @@ bool digit_segments[][DIGIT_LED_SEGMENTS] = {
   { 0,0,0,0,0,0,0 },  
 };
 
-byte lcd_digit_start_segment[] = { 12, 19, 27, 35 };
+byte SCR_lcd_digit_start_segment[] = { 12, 19, 27, 35 };
 
 /// Encoder and buttons
-const int encoder_pin_a = 6;
-const int encoder_pin_b = 5;
-const int push_buttons_pin = A0;
-const int mm_sample_delay = 100;
-const int debouncer_ms = 5;
-const int keyrepeat_ms = 500;
+const int KBD_encoder_pin_a = 6;
+const int KBD_encoder_pin_b = 5;
+const int KBD_push_buttons_pin = A0;
+const int KBD_adc_sample_delay_ms = 100;
+const int KBD_debouncer_ms = 5;
+const int KBD_keyrepeat_ms = 500;
 
-unsigned long last_mm_sample_time = 0;
-unsigned long debounce_millis = 0;
-unsigned long last_key_press = 0;
+unsigned long KBD_last_mm_sample_time = 0;
+unsigned long KBD_debounce_millis = 0;
+unsigned long KBD_last_key_press = 0;
 
-boolean encoder_a_l = LOW;
-boolean encoder_b_l = LOW;
+boolean KBD_encoder_a_l = LOW;
+boolean KBD_encoder_b_l = LOW;
 
 
-void draw_number(byte number, byte digit) {
-  byte start_segment = lcd_digit_start_segment[digit];
-  for (byte seg = 0; seg < DIGIT_LED_SEGMENTS; seg++) {
-    screen_buf[seg + start_segment] = digit_segments[number][seg];
+void SCR_draw_numer(byte number, byte digit) {
+  if (number > 10 || digit > 3) {
+    return;
+  }
+  byte start_segment = SCR_lcd_digit_start_segment[digit];
+  for (byte seg = 0; seg < SCR_DIGIT_LED_SEGMENTS; seg++) {
+    SCR_buf[seg + start_segment] = SCR_digit_segments[number][seg];
   }
 }
 
-void draw_screen_fast() {
+void SCR_draw_fast() {
   byte pinIo =    0; // Everything is INPUT
   byte pinVal =   0; // Everything is LOW
   byte currBit =  1;
   
-  for (byte j = 0; j < LED_PINS_COUNT; j++) {
-    if (current_scan_i == j) {
+  for (byte j = 0; j < SCR_LED_PINS_COUNT; j++) {
+    if (SCR_current_scan_i == j) {
       // Turn the current scanline on
       // Set it as OUTPUT HIGH
       pinIo |= currBit;
       pinVal |= currBit;
     } else {
-      if (screen_buf[ screen_segment[current_scan_offset + j] ]) {
+      if (SCR_buf[ SCR_segment[SCR_current_scan_offset + j] ]) {
         // Force Turn the segment on
         // Set it as OUTPUT LOW
         pinIo |= currBit;
@@ -124,42 +122,42 @@ void draw_screen_fast() {
   DDRB = (DDRB & 1) | (pinIo & B11111110);
   PORTB = (PORTB & 1) | (pinVal & B11111110);
 
-  DDRE = (DDRE & ~PORTE_POS_MASK) | ((pinIo & 1) << PORTE_POS);
-  PORTE = (PORTE & ~PORTE_POS_MASK) | ((pinVal & 1) << PORTE_POS);
+  DDRE = (DDRE & ~SCR_PORTE_POS_MASK) | ((pinIo & 1) << SCR_PORTE_POS);
+  PORTE = (PORTE & ~SCR_PORTE_POS_MASK) | ((pinVal & 1) << SCR_PORTE_POS);
   
-  current_scan_i = (current_scan_i + 1) % LED_PINS_COUNT;
-  current_scan_offset = current_scan_i * 7;
+  SCR_current_scan_i = (SCR_current_scan_i + 1) % SCR_LED_PINS_COUNT;
+  SCR_current_scan_offset = SCR_current_scan_i * 7;
 }
 
-void draw_screen() {
-  for (byte i = 0; i < LED_PINS_COUNT; i++) {
-    pinMode(ledPins[i], INPUT);
+void SCR_draw() {
+  for (byte i = 0; i < SCR_LED_PINS_COUNT; i++) {
+    pinMode(SCR_led_pins[i], INPUT);
   }
   
-  for (byte j = 0; j < LED_PINS_COUNT; j++) {
-    if (current_scan_i == j) {
-      pinMode(ledPins[current_scan_i], OUTPUT);
-      digitalWrite(ledPins[current_scan_i], HIGH);
+  for (byte j = 0; j < SCR_LED_PINS_COUNT; j++) {
+    if (SCR_current_scan_i == j) {
+      pinMode(SCR_led_pins[SCR_current_scan_i], OUTPUT);
+      digitalWrite(SCR_led_pins[SCR_current_scan_i], HIGH);
     } else {
-      if (screen_buf[ screen_segment[current_scan_offset + j] ]) {
-        pinMode(ledPins[j], OUTPUT);
-        digitalWrite(ledPins[j], LOW);
+      if (SCR_buf[ SCR_segment[SCR_current_scan_offset + j] ]) {
+        pinMode(SCR_led_pins[j], OUTPUT);
+        digitalWrite(SCR_led_pins[j], LOW);
       }
     }
   }
   
-  current_scan_i = (current_scan_i + 1) % LED_PINS_COUNT;
-  current_scan_offset = current_scan_i * 7;
+  SCR_current_scan_i = (SCR_current_scan_i + 1) % SCR_LED_PINS_COUNT;
+  SCR_current_scan_offset = SCR_current_scan_i * 7;
 }
 
 
-void setup_keyboard() {
-  pinMode(encoder_pin_a, INPUT);
-  pinMode(encoder_pin_b, INPUT);
-  pinMode(push_buttons_pin, INPUT);
-  digitalWrite(encoder_pin_a, HIGH);
-  digitalWrite(encoder_pin_b, HIGH);
-  digitalWrite(push_buttons_pin, HIGH);
+void KBD_setup() {
+  pinMode(KBD_encoder_pin_a, INPUT);
+  pinMode(KBD_encoder_pin_b, INPUT);
+  pinMode(KBD_push_buttons_pin, INPUT);
+  digitalWrite(KBD_encoder_pin_a, HIGH);
+  digitalWrite(KBD_encoder_pin_b, HIGH);
+  digitalWrite(KBD_push_buttons_pin, HIGH);
 
   digitalWrite(A0, HIGH);
   ADCSRA =  bit (ADEN);                                // turn ADC on
@@ -170,9 +168,9 @@ void setup_keyboard() {
   Consumer.begin();
 }
 
-void process_multimedia_keys()
+void KBD_handle_multimedia_keys()
 {
-  if ( _millis < (last_mm_sample_time + mm_sample_delay) ) {
+  if ( _millis < (KBD_last_mm_sample_time + KBD_adc_sample_delay_ms) ) {
     return;
   }
   
@@ -180,154 +178,135 @@ void process_multimedia_keys()
   unsigned int pushBtnRead = ADC;
   if (pushBtnRead <= 264) {
     if (pushBtnRead > 200) {
-      press_key_no_repeat(MEDIA_NEXT); // Next
+      KBD_press_key_no_repeat(MEDIA_NEXT); // Next
     } else if (pushBtnRead > 150) {
-      press_key_no_repeat(MEDIA_PREVIOUS); // Prev
+      KBD_press_key_no_repeat(MEDIA_PREVIOUS); // Prev
     } else if (pushBtnRead > 115) {
-      press_key_no_repeat(MEDIA_STOP); // Stop/M
+      KBD_press_key_no_repeat(MEDIA_STOP); // Stop/M
     } else {
-      press_key_no_repeat(MEDIA_PLAY_PAUSE); // Play pause
+      KBD_press_key_no_repeat(MEDIA_PLAY_PAUSE); // Play pause
     }
   }
-  last_mm_sample_time = _millis;
+  KBD_last_mm_sample_time = _millis;
 }
 
-void draw_4digits(unsigned int num) {
-  draw_number(num / 1000, 0);
-  draw_number((num % 1000) / 100, 1);
-  draw_number((num % 100) / 10, 2);
-  draw_number(num % 10, 3);
+void SCR_draw_4digits(unsigned int num) {
+  SCR_draw_numer(num / 1000, 0);
+  SCR_draw_numer((num % 1000) / 100, 1);
+  SCR_draw_numer((num % 100) / 10, 2);
+  SCR_draw_numer(num % 10, 3);
 }
 
-void setup_screen() {
+void SCR_setup() {
   // Empty the screen buffer
-  for (byte i = 0; i < SCREEN_SEGMENTS; i++) {
-    screen_buf[i] = false;
+  for (byte i = 0; i < SCR_SEGMENTS; i++) {
+    SCR_buf[i] = false;
   }
-  draw_4digits(8888);
+  SCR_draw_4digits(8888);
 }
 
-void press_key(uint16_t k) {
+void KBD_press_key(uint16_t k) {
   Serial.print(k);
     Consumer.write(k);
 }
 
-void press_key_no_repeat(uint16_t k) {
-  if ( _millis > (last_key_press + keyrepeat_ms) ) {
-    press_key(k);
-    last_key_press = _millis;
+void KBD_press_key_no_repeat(uint16_t k) {
+  if ( _millis > (KBD_last_key_press + KBD_keyrepeat_ms) ) {
+    KBD_press_key(k);
+    KBD_last_key_press = _millis;
   }
 }
 
-
-void process_keyboard_events() {
-  if ( _millis < debounce_millis + debouncer_ms) {
+void KBD_handle_volume_knob() {
+  if ( _millis < KBD_debounce_millis + KBD_debouncer_ms) {
     return;
   }
-  
-  boolean encoder_a = digitalRead(encoder_pin_a);
-  boolean encoder_b = digitalRead(encoder_pin_b);
+
+  boolean encoder_a = digitalRead(KBD_encoder_pin_a);
+  boolean encoder_b = digitalRead(KBD_encoder_pin_b);
 
   if ((encoder_a == HIGH) && (encoder_b == HIGH)) {
-    if ((encoder_a_l == LOW) && (encoder_b_l == HIGH)) {
+    if ((KBD_encoder_a_l == LOW) && (KBD_encoder_b_l == HIGH)) {
       press_key(MEDIA_VOL_UP);      
-    } else if ((encoder_b_l == LOW) && (encoder_a_l == HIGH)) {
+    } else if ((KBD_encoder_b_l == LOW) && (KBD_encoder_a_l == HIGH)) {
       press_key(MEDIA_VOL_DOWN);
     }
   }
   
-  encoder_a_l = encoder_a;
-  encoder_b_l = encoder_b;
+  KBD_encoder_a_l = encoder_a;
+  KBD_encoder_b_l = encoder_b;
 
-  /****************************************/
-  debounce_millis = _millis;
-  process_multimedia_keys();
+  KBD_debounce_millis = _millis;
 }
 
-void setup() {
-  setup_screen();
-  setup_keyboard();
-  Serial.begin(115200);
-  //Serial.write("Screen v2\n");
-  pinMode(indicator_led, OUTPUT);
-  //digitalWrite(indicator_led, LOW); // This LED is using a lot of power...
-}
 
-void loop() {
-  // I have checked and this LowPower actually makes the uC sleep for exactly 1ms
-  // I don't know where the bug is in this library
-  
-  LowPower.idle(SLEEP_15MS, ADC_ON, TIMER4_OFF, TIMER3_OFF, TIMER1_OFF, 
-                  TIMER0_OFF, SPI_OFF, USART1_ON, TWI_OFF, USB_ON);
-                  
-  process_keyboard_events();
-  process_screen_events();
-  process_serial();
-  _millis += 1;
+void KBD_events_process() {
+  KBD_handle_volume_knob();
+  KBD_handle_multimedia_keys();
 }
 
 void spin_circle() {
-  if (screen_spin_circle) {
+  if (SCR_spin_circle) {
     byte circle_segment = _millis / 250 % 3;
     for (byte i = 0; i < 3; i++) {
-      screen_buf[5 + i] = (circle_segment == i);
-      screen_buf[8 + i] = (circle_segment == i);
+      SCR_buf[5 + i] = (circle_segment == i);
+      SCR_buf[8 + i] = (circle_segment == i);
     };
   }
 }
 
-void clear_spin_circle() {
+void SCR_clear_spin_circle() {
   for (byte i = 0; i < 3; i++) {
-    screen_buf[5 + i] = false;
-    screen_buf[8 + i] = false;
+    SCR_buf[5 + i] = false;
+    SCR_buf[8 + i] = false;
   };  
 }
 
-void blink_segments() {
-  if (screen_blink_colon) {
-    screen_buf[26] = _millis / 500 % 2;
+void SCR_blink_segments() {
+  if (SCR_blink_colon) {
+    SCR_buf[26] = _millis / 500 % 2;
   }
 
-  if (screen_blink_dot) {
-    screen_buf[34] = _millis / 500 % 2;
+  if (SCR_blink_dot) {
+    SCR_buf[34] = _millis / 500 % 2;
   }
 }
 
 
-void process_screen_events() {
+void SCR_events_process() {
   if( _millis % 50 == 0 ) {
-    if (screen_draw_clock) {
+    if (SCR_draw_clock) {
       byte secs = _millis / 1000 % 60;
       byte minutes = _millis / 1000 / 60 % 60;
       byte hours = _millis / 1000 / 60 / 60 % 24;
-      draw_4digits(hours*100 + minutes);
+      SCR_draw_4digits(hours*100 + minutes);
     }
-    blink_segments();
+    SCR_blink_segments();
     spin_circle();
   }
-  draw_screen_fast();
+  SCR_draw_fast();
 }
 
-void consume_serial(byte b) {
-  if (serial_state == BYTE_START_MAGIC) {
-    if (b != SERIAL_BYTE_START_MAGIC) {
-      Serial.write(SERIAL_BYTE_ACK_FAIL);
+void SER_consume_char(byte b) {
+  if (SER_state == BYTE_START_MAGIC) {
+    if (b != SER_BYTE_START_MAGIC) {
+      Serial.write(SER_BYTE_ACK_FAIL);
     } else {
-      serial_state ++;
+      SER_state ++;
     }
   } else {
-    serial_cmd[serial_state - 1] = b;
-    serial_state = (serial_state + 1) % (SERIAL_CMD_FIXED_LENGTH + 1);
-    if (serial_state == BYTE_START_MAGIC) {
-      Serial.write(SERIAL_BYTE_ACK_OK);
-      serial_process_command();
+    SER_cmd[SER_state - 1] = b;
+    SER_state = (SER_state + 1) % (SER_CMD_FIXED_LENGTH);
+    if (SER_state == BYTE_START_MAGIC) {
+      Serial.write(SER_BYTE_ACK_OK);
+      SER_process_command();
     }
   }
 }
 
-void serial_process_command() {
+void SER_process_command() {
   //Serial.write("[Got command: ");
-  //Serial.write(serial_cmd, SERIAL_CMD_FIXED_LENGTH);
+  //Serial.write(SER_cmd, SER_CMD_FIXED_LENGTH);
   //Serial.write("\n]");
 
   // Commands
@@ -335,66 +314,150 @@ void serial_process_command() {
   // Set time:           THHMM (24 hours)
   // Set Precise time:   PBBBB (bytes)
   // Display number:     DNNNN
+  // Display digit:      GpN (p=0..3, N=0..9-)
   // Draw spinning disc: Sb (b=0|1)
   // Toggle screen bit:  BPPb (PP is pixel number, b=0|1)
   // Set raw display bit:Rlbbb (l=0|1 low or high, b=byte)
   
   // So send something like CC100\n will draw the clock
 
-  byte cmd = serial_cmd[0];
-  if (cmd == 'C') {
-    screen_draw_clock = (serial_cmd[1] == '1');
-    screen_blink_colon = screen_draw_clock ;
-  } else if (cmd == 'S') {
-    screen_spin_circle = (serial_cmd[1] == '1');
-    if (!screen_spin_circle) { 
-      clear_spin_circle();
-    }
-  } else if (cmd == 'T' || cmd == 'P' || cmd == 'D') {
-    unsigned long offset_char = 0;
-    if (cmd != 'P') {
-      offset_char = '0';
-    }
-    unsigned long hh1 = serial_cmd[1]-offset_char;
-    unsigned long hh2 = serial_cmd[2]-offset_char;
-    unsigned long mm1 = serial_cmd[3]-offset_char;
-    unsigned long mm2 = serial_cmd[4]-offset_char;
+  byte cmd = SER_cmd[0];
+  
+  switch (cmd) {
+    case 'C':
+      SER_process_drawclock(cmd);
+      break;
+      
+    case 'S':
+      SER_process_spin_circle(cmd);
+      break;
 
-    if (cmd == 'D') {
-      screen_draw_clock = false;
-      screen_blink_colon = false;
-      draw_4digits(hh1 * 1000 + hh2 * 100 + mm1 * 10 + mm2);
-      return;
-    }
+    case 'G':
+      SER_process_display_digit(cmd);
+      break;
 
+    case 'B':
+      SER_process_display_bit(cmd);
+      break;
+      
+    case 'R':
+      SER_process_raw_buffer_data(cmd);
+      break;
+      
+    case 'T':
+    case 'P':
+    case 'D':
+      SER_process_time_or_number(cmd);
+      break;
+      
+    default:
+      break;
+  }
+}
+
+void SER_process_raw_buffer_data(byte cmd) {
+  byte offset = SER_cmd[1];
+  for (byte i = 0; i < 3; i++) {
+    unsigned long _byte = SER_cmd[2 + i];
+    for (byte j = 0; j < 8; j++) {
+      byte dest_offset = offset * 24 + i * 8 + j;
+      if (dest_offset >= SCR_SEGMENTS) {
+        return;
+      }
+      SCR_buf[dest_offset] = _byte & 0x1;
+      _byte = _byte >> 1;
+    }
+  }
+}
+
+void SER_process_time_or_number(byte cmd) {  
+  byte offset_char = '0';
+  if (cmd == 'P') {
+    offset_char = 0;
+  }
+  
+  unsigned long hh1 = SER_cmd[1]-offset_char;
+  unsigned long hh2 = SER_cmd[2]-offset_char;
+  unsigned long mm1 = SER_cmd[3]-offset_char;
+  unsigned long mm2 = SER_cmd[4]-offset_char;
+
+  if (cmd == 'D') {
+    SCR_draw_clock = false;
+    SCR_blink_colon = false;
+    SCR_draw_4digits(hh1 * 1000 + hh2 * 100 + mm1 * 10 + mm2);
+  } else {
     if (cmd == 'T') {
       _millis = ((hh1*10 + hh2) * 60 * 60 + (mm1*10 + mm2) * 60) * 1000;
     } else {
       _millis = (hh1 << 24 | hh1 << 16 | mm1 << 8 | mm2);
     }
 
-    debounce_millis = _millis;
-    last_mm_sample_time = _millis;
-  } else if (cmd == 'B') {
-    unsigned long offset_char = '0';
-    byte pp1 = serial_cmd[1]-offset_char;
-    byte pp2 = serial_cmd[2]-offset_char;
-    byte pos = pp1*10 + pp2;
-    screen_buf[pos] = (serial_cmd[3] == '1');
-  } else if (cmd == 'R') {
-    byte offset = (serial_cmd[1] == 0) ? 0 : 1;
-    for (byte i = 0; i < 3; i++) {
-      unsigned long _byte = serial_cmd[2 + i];
-      for (byte j = 0; j < 8; j++) {
-        screen_buf[offset * 24 + i * 8 + j]=_byte & 0x1;
-        _byte = _byte >> 1;
-      }
+    KBD_debounce_millis = _millis;
+    KBD_last_mm_sample_time = _millis;
+  }
+}
+
+void SER_process_display_bit(byte cmd) {
+  byte offset_char = '0';
+  byte pp1 = SER_cmd[1]-offset_char;
+  byte pp2 = SER_cmd[2]-offset_char;
+  
+  byte pos = pp1*10 + pp2;
+  SCR_buf[pos] = (SER_cmd[3] == '1');
+}
+
+void SER_process_drawclock(byte cmd) {
+  SCR_draw_clock = (SER_cmd[1] == '1');
+  SCR_blink_colon = SCR_draw_clock;
+  SCR_buf[34] = false;
+}
+
+void SER_process_spin_circle(byte cmd) {
+  SCR_spin_circle = (SER_cmd[1] == '1');
+  if (!SCR_spin_circle) { 
+    SCR_clear_spin_circle();
+  }
+}
+
+void SER_process_display_digit(byte cmd) {
+  byte offset_char = '0';
+  byte pos = SER_cmd[1] - offset_char;
+  if (SER_cmd[2] == '-') {
+    SCR_draw_numer(10, pos);
+  } else {
+    SCR_draw_numer(SER_cmd[2] - offset_char, pos);
+  }
+}
+
+void SER_events_process() {
+  while (Serial.available() > 0) {
+    byte c = Serial.read();
+    if (c != '\n') {
+      SER_consume_char(c);
     }
   }
 }
 
-void process_serial() {
-  while (Serial.available() > 0) {
-    consume_serial(Serial.read());
-  }
+void SER_setup() {
+  Serial.begin(115200);
+}
+
+void setup() {
+  SCR_setup();
+  KBD_setup();
+  SER_setup();
+}
+
+void loop() {
+  // I have checked and this LowPower actually makes the uC sleep for exactly 1ms
+  // Somehow if you have the USB data lines connected, then this SLEEP_15MS becomes SLEEP_1MS
+  // I don't know how but I gave up finding out.
+  
+  LowPower.idle(SLEEP_15MS, ADC_ON, TIMER4_OFF, TIMER3_OFF, TIMER1_OFF, 
+                  TIMER0_OFF, SPI_OFF, USART1_ON, TWI_OFF, USB_ON);
+                  
+  KBD_events_process();
+  SCR_events_process();
+  SER_events_process();
+  _millis += 1;
 }
